a42fa6ccdf947739ce64bbd419f366e7002a5636
pdvonzoo <a01088570194@gmail.com>
2019-06-11 01:56:26 +0900

    Feat: shop main&detail page

---
+++ src/Components/Review/Review.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+
+const Review = () => <div>Review</div>;
+export default Review;
--- src/Routes/Shop/Shop.tsx
+++ src/Routes/Shop/Shop.tsx
@@ -1,5 +1,65 @@
 import * as React from "react";
+import styled from "../../Utils/typed-styledCom";
+import ShopImage from "../../Assets/shop1.jpg";
 
-const Shop = () => <div>Shop</div>;
+const ShopWrapper = styled.div``;
+const ShopList = styled.div``;
+const ShopItem = styled.div`
+  float: left;
+  width: 25%;
+  text-align: center;
+  &:after {
+    content: "";
+    display: table;
+    both: clear;
+  }
+`;
+const Image = styled.img`
+  width: 100%;
+`;
+const Title = styled.h3``;
+const Price = styled.p``;
+
+const Shop = () => (
+  <ShopWrapper>
+    <ShopList>
+      <ShopItem>
+        <Image src={ShopImage} />
+        <Title>receip</Title>
+        <Price>3,000원</Price>
+      </ShopItem>
+      <ShopItem>
+        <Image src={ShopImage} />
+        <Title>receip</Title>
+        <Price>3,000원</Price>
+      </ShopItem>
+      <ShopItem>
+        <Image src={ShopImage} />
+        <Title>receip</Title>
+        <Price>3,000원</Price>
+      </ShopItem>
+      <ShopItem>
+        <Image src={ShopImage} />
+        <Title>receip</Title>
+        <Price>3,000원</Price>
+      </ShopItem>
+      <ShopItem>
+        <Image src={ShopImage} />
+        <Title>receip</Title>
+        <Price>3,000원</Price>
+      </ShopItem>
+      <ShopItem>
+        <Image src={ShopImage} />
+        <Title>receip</Title>
+        <Price>3,000원</Price>
+      </ShopItem>
+      <ShopItem>
+        <Image src={ShopImage} />
+        <Title>receip</Title>
+        <Price>3,000원</Price>
+      </ShopItem>
+    </ShopList>
+  </ShopWrapper>
+);
 
 export default Shop;
