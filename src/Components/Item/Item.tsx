e92adf3deb934ce0f4ebf2b8a970feb250c12a58
pdvonzoo <a01088570194@gmail.com>
2019-06-21 17:36:09 +0900

    Feat: delete Product 로직 구현

--- src/Components/Item/Item.tsx
+++ src/Components/Item/Item.tsx
@@ -3,11 +3,11 @@ import { useMutation } from "react-apollo-hooks";
 import { DELETE_PRODUCT, EDIT_PRODUCT } from "./ItemQuery";
 
 const Item = ({ item }: any) => {
-  const deleteProduct = useMutation(DELETE_PRODUCT, {
-    variables: { id: item._id }
+  const removeProduct = useMutation(DELETE_PRODUCT, {
+    variables: { productid: item._id }
   });
   const editProduct = useMutation(EDIT_PRODUCT, {
-    variables: { id: item._id }
+    variables: { productid: item._id }
   });
   return (
     <>
@@ -15,7 +15,12 @@ const Item = ({ item }: any) => {
       <button type="button" onClick={(e: any) => editProduct(e)}>
         Edit
       </button>
-      <button type="button" onClick={(e: any) => deleteProduct(e)}>
+      <button
+        type="button"
+        onClick={async () => {
+          removeProduct();
+        }}
+      >
         DELETE
       </button>
     </>
